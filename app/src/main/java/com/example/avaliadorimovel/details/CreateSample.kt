a421f5ef7cf67210be953e9a2bda22f4c0cc87c1
RolphMc <rmc.rodolfo@gmail.com>
2021-04-05 14:49:39 -0300

    last Update:(In Spinner Fotter)
    Retomando projeto do ponto inicial do último ciclo

---
+++ app/src/main/java/com/example/avaliadorimovel/details/CreateSample.kt
@@ -0,0 +1,10 @@
+package com.example.avaliadorimovel.details
+
+class CreateSample (
+    var paradigm: Boolean,
+    var costImmobile: Float?,
+    var areaImmobile: Int,
+    var parkingSpaces: Int,
+    var finishStandart: Float,
+    var conservationState: Float,
+)
