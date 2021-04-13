1af08ab8ec4877766469882c58ba853494145c90
RolphMc <rmc.rodolfo@gmail.com>
2021-04-13 17:04:27 -0300

    Update UnitTestStatisticalCalculator.kt

---
+++ app/src/test/java/com/avaliadorimovel/UnitTestStatisticalCalculator.kt
@@ -0,0 +1,17 @@
+package com.avaliadorimovel
+
+import com.avaliadorimovel.details.DetailsActivity
+import com.avaliadorimovel.details.repository.SampleItem
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class UnitTestStatisticalCalculator {
+    @Test
+    fun `when calculateFactors is called, return populated listSample`() {
+        val sampleList: ArrayList<SampleItem> = DetailsActivity.
+    }
+}
